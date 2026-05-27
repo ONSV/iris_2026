d6ac9cf0edec7b6b167644d205fdaa439988ced9
Ana Marques <absilvamarques@gmail.com>
2026-05-27 12:35:35 -0300

    Atualiza base do RENAEST para o I2

--- data-raw/renaest/raw_renaest.R
+++ data-raw/renaest/raw_renaest.R
@@ -2,17 +2,17 @@ library(arrow)
 
 # SUBSTITUIR O CAMINHO DE ACORDO COM O USUÁRIO QUE ESTIVER EXECUTANDO
 # O ARQUIVO ORIGINAL É MUITO GRANDE PARA INSERIR EM UM REPOSITÓRIO 
-path_acidentes <- "~/Downloads/RENAEST_DABERTOS_20240712/Acidentes_DadosAbertos_20240712.csv"
-path_vitimas <- "~/Downloads/RENAEST_DABERTOS_20240712/Vitimas_DadosAbertos_20240712.csv"
+path_acidentes <- "~/Downloads/renaest_dabertos_20250412/Acidentes_DadosAbertos_20250412.csv"
+path_vitimas <- "~/Downloads/renaest_dabertos_20250412/Vitimas_DadosAbertos_20250412.csv"
 
 renaest_acidentes <- read_csv2_arrow(path_acidentes)
 renaest_vitimas <- read_csv2_arrow(path_vitimas)
 
-acidentes_2023 <- renaest_acidentes |> 
-  filter(ano_acidente == 2023)
+acidentes_2024 <- renaest_acidentes |> 
+  filter(ano_acidente == 2024)
 
-vitimas_2023 <- renaest_vitimas |> 
-  filter(ano_acidente == 2023)
+vitimas_2024 <- renaest_vitimas |> 
+  filter(ano_acidente == 2024)
 
-write_parquet(acidentes_2023, "data-raw/renaest/renaest_acidentes_2023.parquet")
-write_parquet(vitimas_2023, "data-raw/renaest/renaest_vitimas_2023.parquet")
+write_parquet(acidentes_2024, "data-raw/renaest/renaest_acidentes_2024.parquet")
+write_parquet(vitimas_2024, "data-raw/renaest/renaest_vitimas_2024.parquet")
