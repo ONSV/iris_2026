ac7cbf3668cca8d670f772cc6e4d81d755952b2f
Ana Marques <absilvamarques@gmail.com>
2026-06-03 22:06:45 -0300

    feat: altera filtro de mortes para o ano de 2024

--- R/import_datasus_sim.R
+++ R/import_datasus_sim.R
@@ -1,7 +1,7 @@
 import_datasus_sim <- function(pkg_df) {
   pkg_df |> 
     mutate(cod_uf = str_sub(cod_municipio_ocor, 1, 2)) |> 
-    filter(ano_ocorrencia == 2023) |> 
+    filter(ano_ocorrencia == 2024) |> 
     select(cod_uf, nome_uf_ocor) |> 
     count(cod_uf, nome_uf_ocor, name = "obitos") |> 
     drop_na()
