69e8c2967776a2e3e9d747e30d950c5132b736d7
Ana Marques <absilvamarques@gmail.com>
2026-06-22 17:56:10 -0300

    Pilar V: adiciona e calcula novo indicador V.5

--- R/indicadores_pilar_5.R
+++ R/indicadores_pilar_5.R
@@ -13,11 +13,12 @@ calc_indicadores_pilar5 <- function(df_joined) {
             v.1 = n_profissionais / populacao * 1000,
             v.2 = n_leitos / populacao * 1000,
             v.3 = n_leitos_sus / populacao * 1000,
-            v.4 = n_leitos_nao_sus / populacao * 1000
+            v.4 = n_leitos_nao_sus / populacao * 1000,
+            v.5 = n_unidades_moveis / populacao * 1000
         ) |> 
-        select(cod_uf, nome_uf, v.1:v.4) |> 
+        select(cod_uf, nome_uf, v.1:v.5) |> 
         pivot_longer(
-            cols = v.1:v.4,
+            cols = v.1:v.5,
             names_to = "indicador",
             values_to = "valor"
         )
