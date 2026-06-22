82273a2f78c0c8e9368096d219449d4af9d2b59b
Ana Marques <absilvamarques@gmail.com>
2026-06-22 18:06:31 -0300

    Resultado: altera dados de taxa de óbitos por bilhão de quilômetro percorrido

--- R/import_taxabilhao.R
+++ R/import_taxabilhao.R
@@ -1,17 +1,18 @@
 create_taxabilhao <- function() {
   tibble(
     nome_uf = c(
-      "São Paulo", "Distrito Federal", "Rio de Janeiro", "Rio Grande do Sul",
-      "Santa Catarina", "Amapá", "Amazonas", "Roraima", "Minas Gerais",
-      "Rondônia", "Goiás", "Rio Grande do Norte", "Paraná", "Acre",
-      "Mato Grosso", "Mato Grosso do Sul", "Espírito Santo", "Ceará", "Pará",
-      "Tocantins", "Pernambuco", "Bahia", "Sergipe", "Maranhão", "Paraíba",
-      "Alagoas", "Piauí"
+        "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", 
+        "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", 
+        "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", 
+        "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", 
+        "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", 
+        "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
     ),
     taxa_bilhao = c(
-      19.83, 20.00, 28.83, 25.56, 26.29, 20.95, 29.25, 26.85, 28.76, 32.48, 
-      33.80, 33.02, 33.41, 31.96, 35.73, 36.70, 38.91, 43.09, 44.31, 42.48, 
-      50.57, 46.72, 47.48, 54.13, 54.66, 73.75, 71.12
+        25.95, 62.39, 25.82, 26.96, 48.33, 37.13, 14.64, 40.23,
+        28.90, 48.43, 30.18, 28.13, 23.37, 39.97, 46.17, 29.61, 47.92,
+        67.03, 20.11, 28.93, 23.35, 38.14, 29.04, 22.13, 15.57, 50.99, 
+        43.91
     )
   )
 }
